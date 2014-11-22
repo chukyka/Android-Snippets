4fd54e1f44e030edba117f0f558ef2d2faffa935
chukyka <germanka@outlook.com>
2014-11-22 02:40:38 -0300

    Create Loop.java
    This is a loop that runs every x milliseconds

---
+++ Loop.java
@@ -0,0 +1,23 @@
+final Handler mHandler = new Handler();
+       
+int x; // milliseconds
+new Thread(new Runnable() {
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        while (true) {
+            try {
+                Thread.sleep(x);
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        // Write your code here to update the UI.
+                    }
+                });
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    }
+}).start();
